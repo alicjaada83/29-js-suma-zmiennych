227f9b75896fcb0f665e1d77876363596dbfb35f
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 18:55:34 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = '23';
+let secondNumber = '45';
+
+function add(firstNumber, secondNumber) {
+    let suma = firstNumber + secondNumber;
+    return suma;
+}
+console.log( add(23, 45) );
+
+let wynikdodawania = add(23, 45);
+console.log(wynikdodawania);
